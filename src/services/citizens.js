b0974feace6fc9d75f357fad86408d313af8dd14
Beatriz Morera <beatrizmorera2016@gmail.com>
2020-02-12 13:40:45 +0100

    Updates

--- src/services/citizens.js
+++
@@ -1,7 +0,0 @@
-const SERVICE_URL = process.env.REACT_APP_SERVICE_URL;
-
-export async function getCitizens() {
-  const res = await fetch(SERVICE_URL);
-  const citizens = await res.json();
-  return citizens.Brastlewark;
-}
